c5023e58dd4d73dc0a75087398d22a0c9e440635
vihus <vi.hu.sc@gmail.com>
2023-12-01 21:45:40 -0600

    modified:   src/style.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BlogApp } from './BlogApp.jsx'
 import './style.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <body>
       
       <BlogApp />
     </body>
-  </React.StrictMode>,
+  </BrowserRouter>,
 )
